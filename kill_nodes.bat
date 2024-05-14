b92682a4b6245fd1df3a78a44a69abc102f03116
jiachengli <jili5802@uni.sydney.edu.au>
2024-05-15 00:15:51 +1000

    x

--- kill_nodes.bat
+++ kill_nodes.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%p in (8888, 8889, 8890, 8891, 8892. 8893) do (
+for %%p in (8000, 8001, 8002, 8003) do (
     for /f "tokens=5" %%i in ('netstat -aon ^| findstr :%%p ^| findstr LISTENING') do (
         echo Killing process on port %%p with PID %%i
         taskkill /PID %%i /F
